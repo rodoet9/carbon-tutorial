d62fc22bea0b1e3137fb26ea27817b5ad75770a2
Teodor Badarcea <teodor.badarcea@bearingpoint.com>
2023-06-29 16:46:48 +0300

    feat(tutorial): complete step 1

---
+++ src/content/LandingPage/LandingPage.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return <div> Landing Page content appears here</div>;
+};
+export default LandingPage;
---
+++ src/content/RepoPage/RepoPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RepoPage = () => {
+  return <div>Repo Page content appears here</div>;
+};
+
+export default RepoPage;
